7f6c6366f6cf033c5fdc83720148fa62ca1f892c
Nick Vigier <nvigier46@gmail.com>
2024-08-06 09:12:42 +0200

    testing preview environment variables with build cache enabled

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main>{process.env.NEXT_PUBLIC_TEST}</main>;
+  return (
+    <main>process.env.NEXT_PUBLIC_TEST: {process.env.NEXT_PUBLIC_TEST}</main>
+  );
 }
